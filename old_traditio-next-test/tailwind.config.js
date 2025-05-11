ea8dbba8ed228ef83d864aea7e7272237e61ab8c
AutoJenny <github@nickfiddes.com>
2025-05-11 12:57:02 +0100

    test

---
+++ old_traditio-next-test/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx,mdx,html}'],
+  theme: {
+    extend: {
+      colors: {
+        ivory: '#F8F5F0',
+        espresso: '#3A2E2B',
+        sand: '#D6CFC6',
+        brass: '#BFA07A',
+        clay: '#A28E7D',
+      },
+      fontFamily: {
+        heading: ['Playfair Display', 'serif'],
+        body: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
